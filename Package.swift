2d23f587acf7ec0e6163e6a082962538f27706d2
Aurelius Prochazka <aurephd@gmail.com>
2021-06-07 00:15:23 -0700

    Using AudioKit version instead of branch name

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "DevoloopAudioKit",
     platforms: [.macOS(.v10_14), .iOS(.v13), .tvOS(.v13)],
     products: [.library(name: "DevoloopAudioKit", targets: ["DevoloopAudioKit"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", .branch("devolop"))],
+    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.2.0")],
     targets: [
         .target(name: "DevoloopAudioKit", dependencies: ["AudioKit", "CDevoloopAudioKit"]),
         .target(name: "CDevoloopAudioKit", dependencies: ["AudioKit"]),
