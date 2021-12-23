49798ad1b4016148599b65c15b42ea45ab1a90d0
Aurelius Prochazka <aurephd@gmail.com>
2021-12-22 16:58:15 -0800

    Updated to AK 5.3

--- Package.swift
+++ Package.swift
@@ -7,10 +7,13 @@ let package = Package(
     name: "DevoloopAudioKit",
     platforms: [.macOS(.v10_13), .iOS(.v11), .tvOS(.v11)],
     products: [.library(name: "DevoloopAudioKit", targets: ["DevoloopAudioKit"])],
-    dependencies: [.package(url: "https://github.com/AudioKit/AudioKit", from: "5.2.0")],
+    dependencies: [
+        .package(url: "https://github.com/AudioKit/AudioKit", from: "5.3.0"),
+        .package(url: "https://github.com/AudioKit/AudioKitEX", from: "5.3.0"),
+    ],
     targets: [
-        .target(name: "DevoloopAudioKit", dependencies: ["AudioKit", "CDevoloopAudioKit"]),
-        .target(name: "CDevoloopAudioKit", dependencies: ["AudioKit"]),
+        .target(name: "DevoloopAudioKit", dependencies: ["AudioKit", "AudioKitEX", "CDevoloopAudioKit"]),
+        .target(name: "CDevoloopAudioKit", dependencies: ["AudioKit", "AudioKitEX"]),
         .testTarget(name: "DevoloopAudioKitTests", dependencies: ["DevoloopAudioKit"], resources: [.copy("TestResources/")]),
     ],
     cxxLanguageStandard: .cxx14
